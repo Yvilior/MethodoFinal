fb4d440bf900ba04625fb3da2d50a93c72464925
Yvain Guillon <yvainmacguire@icloud.com>
2026-05-05 09:24:28 -0400

    ajout de mon nom dans le projet

--- src/game.cpp
+++ src/game.cpp
@@ -6,7 +6,7 @@ GameApp::GameApp ()
 	if (SDL_Init (SDL_INIT_VIDEO) == false)
 		abort ();
 
-	Window = SDL_CreateWindow ("App", 800, 600, 0);
+	Window = SDL_CreateWindow ("Guillon Yvain Exam Final", 800, 600, 0);
 	if (Window == nullptr)
 		abort ();
 
